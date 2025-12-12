ecf61e0406bf09adcee3f169955338ff5352fe90
asyockss <nastya28042020@gmail.com>
2025-12-12 14:44:38 +0500

    Получение информации с сайта.Черных

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,17 @@ namespace HttpNewsPAT
     {
         static void Main(string[] args)
         {
-
+            WebRequest request = WebRequest.Create("http://10.111.20.114/main.php");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.StatusDescription);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            Console.Read();
         }
     }
 }
