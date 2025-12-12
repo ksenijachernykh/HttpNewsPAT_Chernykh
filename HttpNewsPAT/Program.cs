304e03f71c2ab110e7bb35eadd22bb1440bce651
asyockss <nastya28042020@gmail.com>
2025-12-12 14:58:28 +0500

    Получение печеньки авторизации.Черных

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -12,7 +13,8 @@ namespace HttpNewsPAT
     {
         static void Main(string[] args)
         {
-            WebRequest request = WebRequest.Create("http://10.111.20.114/main.php");
+            SingIn("user", "user");
+            /*WebRequest request = WebRequest.Create("http://10.111.20.114/main.php");
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             Console.WriteLine(response.StatusDescription);
             Stream dataStream = response.GetResponseStream();
@@ -21,8 +23,30 @@ namespace HttpNewsPAT
             Console.WriteLine(responseFromServer);
             reader.Close();
             dataStream.Close();
-            response.Close();
+            response.Close();*/
             Console.Read();
         }
+
+        public static void SingIn(string Login, string Password)
+        {
+            string url = "http://10.111.20.114/ajax/login.php";
+            Debug.WriteLine($"Выполняем запрос: {url}");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CookieContainer = new CookieContainer();
+            string postData = $"login={Login}&password={Password}";
+            byte[] Data = Encoding.ASCII.GetBytes(postData);
+            request.ContentLength = Data.Length;
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(Data, 0, Data.Length);
+            }
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+            string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            Console.WriteLine(responseFromServer);
+        }
+
     }
 }
